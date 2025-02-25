3d9a3f922b203d55098350e1338f93ce2b054fff
carolinedevine <cdevine@colgate.edu>
2025-02-24 22:51:59 -0500

    update

--- Lab5.R
+++ Lab5.R
@@ -94,6 +94,12 @@ selected.data <- final_all |>
   filter(feature %in% select.features)
 view(selected.data) 
 
+isolated.feature.sum <- selected.data |>
+  filter(feature %in% c("average_loudness", "chords_strength", "dissonance", "spectral_rolloff",
+                        "positivewords", "OtherP", "Perception", "conj")) |>
+  select(artist, min, LF, UF, max, description, feature)
+view(isolated.feature.sum)
+
 # isolate the artist, feature, and description (within range, outlying, or out of range)
 select.data <- selected.data |>
   select("artist", "feature", "description")
@@ -101,7 +107,7 @@ view(select.data)
 
 isolated.features <- select.data |>
   filter(feature %in% c("average_loudness", "chords_strength", "dissonance", "spectral_rolloff",
-                        "positivewords", "OtherP", "Perception", "conj"))
+                        "positivewords", "OtherP", "Perception", "conj")) 
 view(isolated.features)
 
 library("xtable")
@@ -109,12 +115,137 @@ table <- xtable(isolated.features)
 view(table)
 print(table, include.rownames = F) # 24 rows (slightly large) but 4 features from LIWC and 4 features from Essentia
 
+sum.table <- xtable(isolated.feature.sum)
+view(sum.table)
+print(sum.table, include.rownames = F) # 24 rows (slightly large) but 4 features from LIWC and 4 features from Essentia
+
+
 
 ################################################################################
 # Step 4: Graphing Selected Features
 ################################################################################
 library(tidyverse)
 library(patchwork)
+library(ggplot2)
+
+view(essentia.data)
+view(essentia.data.allentown)
+
+### Essentia Data: Sound ###
+
+# Dissonance
+dissonance.plot <- ggplot(data=essentia.data,                       # Specify the data
+                aes(x=artist, y=dissonance)) +         # Specify x and y
+        geom_violin(fill="grey80")+
+        geom_boxplot(width = 0.1) +
+        theme_bw()+                                       # Remove the grey background
+        xlab("Artist")+                                   # Label x axis
+        ylab("Dissonance")+                           # Label y axis
+        geom_hline(yintercept = essentia.data.allentown$dissonance, 
+                  linetype = "dotted", linewidth = 1)
+dissonance.plot
+
+
+# Average Loudness
+average.plot <- ggplot(data=essentia.data,                       # Specify the data
+                aes(x=artist, y=average_loudness)) +         # Specify x and y
+  geom_violin(fill="grey80")+
+  geom_boxplot(width = 0.1) +
+  theme_bw()+                                       # Remove the grey background
+  xlab("Artist")+                                   # Label x axis
+  ylab("Average Loudness")+                           # Label y axis
+  geom_hline(yintercept = essentia.data.allentown$average_loudness, 
+             linetype = "dotted", linewidth = 1)
+average.plot
+
+# Chords Strength
+chords.plot <- ggplot(data=essentia.data,                       # Specify the data
+                       aes(x=artist, y=chords_strength)) +         # Specify x and y
+  geom_violin(fill="grey80")+
+  geom_boxplot(width = 0.1) +
+  theme_bw()+                                       # Remove the grey background
+  xlab("Artist")+                                   # Label x axis
+  ylab("Chords Strength")+                           # Label y axis
+  geom_hline(yintercept = essentia.data.allentown$chords_strength, 
+             linetype = "dotted", linewidth = 1)
+chords.plot
+
+# Spectral Rolloff
+rolloff.plot <- ggplot(data=essentia.data,                       # Specify the data
+                      aes(x=artist, y=spectral_rolloff)) +         # Specify x and y
+  geom_violin(fill="grey80")+
+  geom_boxplot(width = 0.1) +
+  theme_bw()+                                       # Remove the grey background
+  xlab("Artist")+                                   # Label x axis
+  ylab("Spectral Rolloff")+                           # Label y axis
+  geom_hline(yintercept = essentia.data.allentown$spectral_rolloff, 
+             linetype = "dotted", linewidth = 1)
+rolloff.plot
+
+
+#### LIWC Data: Lyrical ####
+
+# Perception
+perception.plot <- ggplot(data=essentia.data,                       # Specify the data
+                       aes(x=artist, y=Perception)) +         # Specify x and y
+  geom_violin(fill="grey80")+
+  geom_boxplot(width = 0.1) +
+  theme_bw()+                                       # Remove the grey background
+  xlab("Artist")+                                   # Label x axis
+  ylab("Perception")+                           # Label y axis
+  geom_hline(yintercept = essentia.data.allentown$Perception, 
+             linetype = "dotted", linewidth = 1)
+perception.plot
+
+# Positive Words
+positive.plot <- ggplot(data=essentia.data,                       # Specify the data
+                          aes(x=artist, y=positivewords)) +         # Specify x and y
+  geom_violin(fill="grey80")+
+  geom_boxplot(width = 0.1) +
+  theme_bw()+                                       # Remove the grey background
+  xlab("Artist")+                                   # Label x axis
+  ylab("Positive Words")+                           # Label y axis
+  geom_hline(yintercept = essentia.data.allentown$positivewords, 
+             linetype = "dotted", linewidth = 1)
+positive.plot
+
+
+# Other Parts of Speech
+other.plot <- ggplot(data=essentia.data,                       # Specify the data
+                        aes(x=artist, y=OtherP)) +         # Specify x and y
+  geom_violin(fill="grey80")+
+  geom_boxplot(width = 0.1) +
+  theme_bw()+                                       # Remove the grey background
+  xlab("Artist")+                                   # Label x axis
+  ylab("OtherP")+                           # Label y axis
+  geom_hline(yintercept = essentia.data.allentown$OtherP, 
+             linetype = "dotted", linewidth = 1)
+other.plot
+
+# Conjunctions
+conj.plot <- ggplot(data=essentia.data,                       # Specify the data
+                      aes(x=artist, y=conj)) +         # Specify x and y
+  geom_violin(fill="grey80")+
+  geom_boxplot(width = 0.1) +
+  theme_bw()+                                       # Remove the grey background
+  xlab("Artist")+                                   # Label x axis
+  ylab("Conjunctions")+                           # Label y axis
+  geom_hline(yintercept = essentia.data.allentown$conj, 
+             linetype = "dotted", linewidth = 1)
+conj.plot
+
+
+### Combine Plots ####
+library(patchwork)
+help(patchwork)
+combined.lyric.plot <- conj.plot + other.plot + positive.plot + perception.plot + plot_annotation(title = "Lyrical Features")
+combined.lyric.plot
+
+combined.sound.plot <- average.plot + dissonance.plot + chords.plot + rolloff.plot + plot_annotation(title = "Sound Features")
+combined.sound.plot
+
+
+# Previous Bad Plots
 
 # #graph.isolated.features <- selected.data |>
 #  # filter(feature %in% c("average_loudness", "chords_strength", "dissonance", "spectral_rolloff",
@@ -147,139 +278,3 @@ library(patchwork)
 #   ggtitle("Sound Features")
 # 
 
-# Box Plot
-
-essentia.data
-view(essentia.data)
-
-library(ggplot2)
-
-view(essentia.data.allentown)
-
-### Essentia Data: Sound ###
-
-# Dissonance
-
-dissonance.plot <- ggplot(data=essentia.data,                       # Specify the data
-                aes(x=artist, y=dissonance)) +         # Specify x and y
-        geom_violin(fill="grey80")+
-        geom_boxplot(width = 0.1) +
-        theme_bw()+                                       # Remove the grey background
-        xlab("Artist")+                                   # Label x axis
-        ylab("Dissonance")+                           # Label y axis
-        geom_hline(yintercept = essentia.data.allentown$dissonance, 
-                  linetype = "dotted", linewidth = 1)
-dissonance.plot
-
-
-# Average Loudness
-
-average.plot <- ggplot(data=essentia.data,                       # Specify the data
-                aes(x=artist, y=average_loudness)) +         # Specify x and y
-  geom_violin(fill="grey80")+
-  geom_boxplot(width = 0.1) +
-  theme_bw()+                                       # Remove the grey background
-  xlab("Artist")+                                   # Label x axis
-  ylab("Average Loudness")+                           # Label y axis
-  geom_hline(yintercept = essentia.data.allentown$average_loudness, 
-             linetype = "dotted", linewidth = 1)
-
-average.plot
-
-# Chords Strength
-
-chords.plot <- ggplot(data=essentia.data,                       # Specify the data
-                       aes(x=artist, y=chords_strength)) +         # Specify x and y
-  geom_violin(fill="grey80")+
-  geom_boxplot(width = 0.1) +
-  theme_bw()+                                       # Remove the grey background
-  xlab("Artist")+                                   # Label x axis
-  ylab("Chords Strength")+                           # Label y axis
-  geom_hline(yintercept = essentia.data.allentown$chords_strength, 
-             linetype = "dotted", linewidth = 1)
-
-chords.plot
-
-# Spectral Rolloff
-
-rolloff.plot <- ggplot(data=essentia.data,                       # Specify the data
-                      aes(x=artist, y=spectral_rolloff)) +         # Specify x and y
-  geom_violin(fill="grey80")+
-  geom_boxplot(width = 0.1) +
-  theme_bw()+                                       # Remove the grey background
-  xlab("Artist")+                                   # Label x axis
-  ylab("Spectral Rolloff")+                           # Label y axis
-  geom_hline(yintercept = essentia.data.allentown$spectral_rolloff, 
-             linetype = "dotted", linewidth = 1)
-
-rolloff.plot
-
-
-#### LIWC Data: Lyrical ####
-
-# Perception
-
-perception.plot <- ggplot(data=essentia.data,                       # Specify the data
-                       aes(x=artist, y=Perception)) +         # Specify x and y
-  geom_violin(fill="grey80")+
-  geom_boxplot(width = 0.1) +
-  theme_bw()+                                       # Remove the grey background
-  xlab("Artist")+                                   # Label x axis
-  ylab("Perception")+                           # Label y axis
-  geom_hline(yintercept = essentia.data.allentown$Perception, 
-             linetype = "dotted", linewidth = 1)
-
-perception.plot
-
-# Positive Words
-
-positive.plot <- ggplot(data=essentia.data,                       # Specify the data
-                          aes(x=artist, y=positivewords)) +         # Specify x and y
-  geom_violin(fill="grey80")+
-  geom_boxplot(width = 0.1) +
-  theme_bw()+                                       # Remove the grey background
-  xlab("Artist")+                                   # Label x axis
-  ylab("Positive Words")+                           # Label y axis
-  geom_hline(yintercept = essentia.data.allentown$positivewords, 
-             linetype = "dotted", linewidth = 1)
-
-positive.plot
-
-
-# Other Parts of Speech
-
-other.plot <- ggplot(data=essentia.data,                       # Specify the data
-                        aes(x=artist, y=OtherP)) +         # Specify x and y
-  geom_violin(fill="grey80")+
-  geom_boxplot(width = 0.1) +
-  theme_bw()+                                       # Remove the grey background
-  xlab("Artist")+                                   # Label x axis
-  ylab("OtherP")+                           # Label y axis
-  geom_hline(yintercept = essentia.data.allentown$OtherP, 
-             linetype = "dotted", linewidth = 1)
-
-other.plot
-
-# Conjunctions
-
-conj.plot <- ggplot(data=essentia.data,                       # Specify the data
-                      aes(x=artist, y=conj)) +         # Specify x and y
-  geom_violin(fill="grey80")+
-  geom_boxplot(width = 0.1) +
-  theme_bw()+                                       # Remove the grey background
-  xlab("Artist")+                                   # Label x axis
-  ylab("Conjunctions")+                           # Label y axis
-  geom_hline(yintercept = essentia.data.allentown$conj, 
-             linetype = "dotted", linewidth = 1)
-
-conj.plot
-
-
-### Combine Plots ####
-library(patchwork)
-help(patchwork)
-combined.lyric.plot <- conj.plot + other.plot + positive.plot + perception.plot + plot_annotation(title = "Lyrical Features")
-combined.lyric.plot
-
-combined.sound.plot <- average.plot + dissonance.plot + chords.plot + rolloff.plot + plot_annotation(title = "Sound Features")
-combined.sound.plot
